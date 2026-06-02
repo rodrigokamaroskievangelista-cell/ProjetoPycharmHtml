5314d6348edf9be16669f6749180b18b636fbf3b
Rodrigo Evangelista <rodrigokamaroskievangelista@gmail.com>
2026-06-02 17:17:54 -0300

    por hoje foi isso

--- templates/app.py
+++ templates/app.py
@@ -18,6 +18,37 @@ def processar_formulario():
  O nome entre colchetes deve ser igual ao "nome" do campo formulario
  HTML """
 
-nome = request.form['nome'] # Captura o campo nome
+nome = request.form['nome']   # Captura o campo nome
 idade = request.form['idade'] # Captura o campo idade
 curso = request.form['curso'] # Captura o campo curso
+
+# Validaçao de dados
+if not nome or not idade or not curso:
+    mensagem_resultado = "ERRO: Todos os campos são Obrigatorios!"
+else:
+    idade_int = int(idade)
+    mensagem_base = f"Ola{nome}, você tem {idade} anos e está no curso de {curso}!"
+if idade_int < 18:
+    mensagem_idade = "Voce e Menor de Idade."
+elif idade_int > 18 and idade_int <65:
+    mensagem_idade = "Voce é Adulto."
+else:
+    mensagem_idade = "Você é experiente!."
+
+# Estrutura condicional para o Curso
+
+    if curso: == "Python":
+    mensagem_curso = "Otima escolha, você é versátil!"
+    elif curso == "Flask":
+    mensagem_curso = "Otima escolha gafanhoto!"
+    elif == "HTML/CSS":
+    mensagem_curso = "Fundamental pequenino gafanhoto"
+else:
+    mensagem_curso = "Curso Interressante"
+    mensagem_resultado = f"{mensagem_curso}\n{mensagem_idade}\n{mensagem_idade}"
+
+    mensagem_resultado = "Sucesso!\n" mensagem_resultado
+
+return render_template("formulario.html", resultado=mensagem_resultado)
+
+if __name__ == '__main__':
