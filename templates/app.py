98f673a39f98aac104d910d2a05c98e6715bb98f
Rodrigo Evangelista <rodrigokamaroskievangelista@gmail.com>
2026-06-02 16:57:44 -0300

    mod

--- templates/app.py
+++ templates/app.py
@@ -8,3 +8,16 @@ def pagina_inicio():
 @app.route('/processar', methods=['POST'])
 def processar_formulario():
     if request.method == 'POST':
+
+""" Esta função recebe os dados enviados pelo formulário,
+ processa-os com estruturas condicionais e retorna
+ uma mensagem personalizada. """
+
+""" Capturas os dados do Formulario
+ request.form e um dicionario com todos os campos enviados
+ O nome entre colchetes deve ser igual ao "nome" do campo formulario
+ HTML """
+
+nome = request.form['nome'] # Captura o campo nome
+idade = request.form['idade'] # Captura o campo idade
+curso = request.form['curso'] # Captura o campo curso
